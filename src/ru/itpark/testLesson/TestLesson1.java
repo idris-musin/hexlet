56f9f054b1a4ef6d81c70bfaa2fdab96f15ec205
idris-musin <musin.idris@gmail.com>
2022-10-02 21:42:56 +0300

    taskLesson1 commit

---
+++ src/ru/itpark/testLesson/TestLesson1.java
@@ -0,0 +1,27 @@
+package ru.itpark.testLesson;
+
+public class TestLesson1 {
+    public static void Test1() {
+
+        System.out.println(isPowerOfThree(1)); // true
+        System.out.println(isPowerOfThree(3)); // true
+        System.out.println(isPowerOfThree(4)); // false
+        System.out.println(isPowerOfThree(9)); // true
+        System.out.println(isPowerOfThree(6)); // false
+        System.out.println(isPowerOfThree(-3)); // false
+        System.out.println(isPowerOfThree(0)); // false
+    }
+
+    public static boolean isPowerOfThree(int number) {
+
+        if (number <1) return false;
+        while (number != 1) {
+            if (number % 3 != 0) {
+                return false;
+            }
+            number = number / 3;
+        }
+
+        return true;
+    }
+}
